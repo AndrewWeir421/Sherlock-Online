aabced63cc07900ad7d0d28b01eedddd402baded
Andrew Weir <andrewweir@weirtech.net>
2022-08-07 16:21:12 -0400

    Now runs the action on GitHub with the specified username.

---
+++ src/scripts/index.js
@@ -0,0 +1,40 @@
+var url = "https://api.github.com/repos/Sherlock-Online/Sherlock-Online.github.io/dispatches";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+var fieldValue = document.getElementById("UsernameForm").value;
+xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
+xhr.setRequestHeader("Authorization", "Bearer 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");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+function sleep(milliseconds) {
+    const start = Date.now();
+    while (Date.now() - start < milliseconds);
+  }
+
+function start(){
+    fieldValue = document.getElementById("UsernameForm").value;
+    warning = document.getElementById("Warning");
+    if (fieldValue == ""){
+        alert("Please Enter A Username!")
+    } else {
+    var data = `{
+        "event_type": "start",
+        "client_payload": {
+            "username": "${fieldValue}"
+        }
+    }`;
+    console.log(fieldValue)
+    console.log(data)
+    xhr.send(data);
+
+    }
+}
+
+
